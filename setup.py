ca50fdf188170da7b376e1879067a61f39ccce36
Nicholas Vadivelu <nicholas.vadivelu@gmail.com>
2020-12-25 18:36:39 -0500

    Configure yapf, isort, and pre-commit

--- setup.py
+++ setup.py
@@ -6,37 +6,41 @@ from setuptools import find_packages, setup
 
 
 def read(*names, **kwargs):
-    with io.open(os.path.join(os.path.dirname(__file__), *names), encoding=kwargs.get("encoding", "utf8")) as fp:
+    with io.open(os.path.join(os.path.dirname(__file__), *names),
+                 encoding=kwargs.get('encoding', 'utf8')) as fp:
         return fp.read()
 
 
 def find_version(*file_paths):
     version_file = read(*file_paths)
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    version_match = re.search(r'^__version__ = ["\']([^"\']*)["\']', version_file, re.M)
     if version_match:
         return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
+    raise RuntimeError('Unable to find version string.')
 
 
-readme = read("README.md")
+readme = read('README.md')
 
-VERSION = find_version("shapecheck", "__init__.py")
+VERSION = find_version('shapecheck', '__init__.py')
 
 requirements = []
 
 setup(
     # Metadata
-    name="shapecheck",
+    name='shapecheck',
     version=VERSION,
-    author="Nicholas Vadivelu",
-    author_email="nicholas.vadivelu@gmail.com",
-    url="https://github.com/n2cholas/shapecheck",
-    description="Framework-agnostic library for checking array shapes at runtime.",
-    long_description_content_type="text/markdown",
+    author='Nicholas Vadivelu',
+    author_email='nicholas.vadivelu@gmail.com',
+    url='https://github.com/n2cholas/shapecheck',
+    description='Framework-agnostic library for checking array shapes at runtime.',
+    long_description_content_type='text/markdown',
     long_description=readme,
-    license="MIT",
+    license='MIT',
     # Package info
-    packages=find_packages(exclude=("tests", "tests.*",)),
+    packages=find_packages(exclude=(
+        'tests',
+        'tests.*',
+    )),
     zip_safe=True,
     install_requires=requirements,
 )
