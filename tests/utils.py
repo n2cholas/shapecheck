5c801936d69d983ec627487d159ec9c783b91216
Nicholas Vadivelu <nicholas.vadivelu@gmail.com>
2020-12-25 23:03:55 -0500

    Add test utils

---
+++ tests/utils.py
@@ -0,0 +1,14 @@
+import sys
+from io import StringIO
+
+
+class CaptureStdOut(list):
+    def __enter__(self):
+        self._stdout = sys.stdout
+        sys.stdout = self._stringio = StringIO()
+        return self
+
+    def __exit__(self, *args):
+        self.extend(self._stringio.getvalue().splitlines())
+        del self._stringio  # free up some memory
+        sys.stdout = self._stdout
