3f624d23b361f84aeaeb0ec22b3a833f3a2b97eb
Nicholas Vadivelu <nicholas.vadivelu@gmail.com>
2020-12-25 22:40:01 -0500

    Add named dimensions.

--- shapecheck/_main.py
+++ shapecheck/_main.py
@@ -1,15 +1,34 @@
-def is_compatible(shape, expected_shape):
-    return shape == expected_shape
+from functools import wraps
+
+
+def is_compatible(shape, expected_shape, dim_dict=None):
+    if dim_dict is None:
+        dim_dict = {}
+    for dim, exp_dim in zip(shape, expected_shape):
+        if isinstance(exp_dim, str):
+            if exp_dim in dim_dict:
+                print('Found')
+                exp_dim = dim_dict[exp_dim]
+            else:
+                print('Adding ', exp_dim, dim, shape, expected_shape)
+                dim_dict[exp_dim] = dim
+                exp_dim = dim
+
+        if exp_dim != -1 and exp_dim is not None and exp_dim != dim:
+            return False
+    return True
 
 
 def check_shape(*in_shapes, out_=None):
     def decorator(f):
+        @wraps(f)
         def inner(*args):
+            dim_dict = {}
             for in_shape, arg in zip(in_shapes, args):
-                assert is_compatible(arg.shape, in_shape)
+                assert is_compatible(arg.shape, in_shape, dim_dict=dim_dict)
             out = f(*args)
             if out_:
-                assert is_compatible(out.shape, out_)
+                assert is_compatible(out.shape, out_, dim_dict=dim_dict)
             return out
 
         return inner
--- tests/test_shapecheck.py
+++ tests/test_shapecheck.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 
 from shapecheck import check_shape
 
@@ -9,3 +10,24 @@ def test_basic():
         return x[:2]**2 + y[:2]**2
 
     func(np.array([1, 2, 3]), np.array([1, 2, 3, 4]))
+    with pytest.raises(AssertionError):
+        func(np.array([1, 2, 3]), np.array([2, 3, 4]))
+
+
+def test_named_dim():
+    @check_shape(('N', 3), ('N',), out_=('N', 1))
+    def func(x, y):
+        return (x + y).sum(-1, keepdims=True)
+
+    func(np.ones((5, 3)), np.ones((5, 1)))
+    with pytest.raises(AssertionError):
+        func(np.ones((4, 3)), np.ones((5, 1)))
+
+
+def test_any_dim():
+    @check_shape(('N', -1), out_=('N', 1))
+    def func(x):
+        return x.sum(-1, keepdims=True)
+
+    func(np.ones((5, 3)))
+    func(np.ones((5, 7)))
